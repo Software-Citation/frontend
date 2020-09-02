df7c3b90ccecd65b0b942af67c32f56709c5a59b
naomiaro <naomiaro@gmail.com>
2020-09-02 12:39:26 +0100

    bootstrap grid

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import styles from './NavBar.module.scss';
 
 function NavBar() {
   return (
-    <Nav className="justify-content-right" defaultActiveKey="home">
+    <Nav className="justify-content-end" defaultActiveKey="home">
       <Nav.Link eventKey="home" href="/">
         Home
       </Nav.Link>
